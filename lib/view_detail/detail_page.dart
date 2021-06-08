f9c0a6ace4c73ef51a4e3eb47f13350851c04f5e
月見里浩太 <moon.rewrite@gmail.com>
2021-06-08 12:37:45 +0900

    [update] refs #1557 API呼び出し

--- lib/view_detail/detail_page.dart
+++ lib/view_detail/detail_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app_api/component/app_color.dart';
+import 'package:flutter_app_api/entity/pokemon_info.dart';
+import 'package:flutter_app_api/view_detail/detail_view_model.dart';
 
 import 'detail_app_bar.dart';
 
@@ -9,12 +11,40 @@ class DetailPage extends StatefulWidget {
 }
 
 class _DetailPage extends State<DetailPage> {
+  late DetailViewModel _detailViewModel;
+
   @override
-  Widget build(BuildContext context) {
-    return _initView();
+  void initState() {
+    super.initState();
+    _detailViewModel = DetailViewModel();
   }
 
-  Widget _initView() {
+  @override
+  Widget build(BuildContext context) {
+    List<String> argumentList = <String>[];
+    if (ModalRoute.of(context) != null && ModalRoute.of(context)!.settings.arguments is List<String>) {
+      argumentList = ModalRoute.of(context)!.settings.arguments as List<String>;
+    }
+    if (argumentList.length != 2) {
+      return _errorWidget();
+    }
+
+    return FutureBuilder(
+        future: _detailViewModel.getPokemonInfo(argumentList[0], argumentList[1]),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            if (_detailViewModel.pokemonInfo != null) {
+              return _initView(_detailViewModel.pokemonInfo!);
+            } else {
+              return _errorWidget();
+            }
+          } else {
+            return const CircularProgressIndicator();
+          }
+        });
+  }
+
+  Widget _initView(PokemonInfo pokemonInfo) {
     return Scaffold(
       backgroundColor: AppColor.BACKGROUND_COLOR,
       appBar: DetailAppBar(),
